da43fb41d49bcb2d04bfce88448334ec38cd1c57
Tulir Asokan <tulir@maunium.net>
2018-08-24 23:06:55 +0300

    Add Dockerfile

---
+++ docker-run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! -f /data/config.yaml ]; then
+	cp /opt/mautrix-whatsapp/example-config.yaml /data/config.yaml
+	echo "Didn't find a config file."
+	echo "Copied default config file to /data/config.yaml"
+	echo "Modify that config file to your liking."
+	echo "Start the container again after that to generate the registration file."
+	exit
+fi
+
+if [ ! -f /data/registration.yaml ]; then
+	/usr/bin/mautrix-whatsapp -g -c /data/config.yaml -r /data/registration.yaml
+	echo "Didn't find a registration file."
+	echo "Generated one for you."
+	echo "Copy that over to synapses app service directory."
+	exit
+fi
+
+cd /data
+/usr/bin/mautrix-whatsapp
