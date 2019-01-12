79bd35964fc6b9db82cd75e5b329d3561da9a505
Tulir Asokan <tulir@maunium.net>
2019-01-12 15:54:04 +0200

    Update license year, Dockerfile, docker-run.sh and deps and add editorconfig

--- types/types.go
+++ types/types.go
@@ -1,5 +1,5 @@
 // mautrix-whatsapp - A Matrix-WhatsApp puppeting bridge.
-// Copyright (C) 2018 Tulir Asokan
+// Copyright (C) 2019 Tulir Asokan
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
