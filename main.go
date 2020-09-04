1edf8bb9e1028620e0f946266d2a933f493b7c20
Tulir Asokan <tulir@maunium.net>
2020-09-04 17:53:53 +0300

    Bump version to v0.1.4

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ var (
 	Name = "mautrix-whatsapp"
 	URL  = "https://github.com/tulir/mautrix-whatsapp"
 	// This is changed when making a release
-	Version   = "0.1.3"
+	Version   = "0.1.4"
 	// This is filled by init()
 	WAVersion = ""
 	// These are filled at build time with the -X linker flag
