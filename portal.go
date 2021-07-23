e2acab694cdf4924a7d5b9516a2da3d67ac704f9
Tulir Asokan <tulir@maunium.net>
2021-07-23 22:44:35 +0300

    Fix handling gifs in stickers

--- portal.go
+++ portal.go
@@ -2087,7 +2087,8 @@ func (portal *Portal) convertMatrixMessage(sender *User, evt *event.Event) (*waP
 	}
 	if evt.Type == event.EventSticker {
 		content.MsgType = event.MsgImage
-	} else if content.MsgType == event.MsgImage && content.GetInfo().MimeType == "image/gif" {
+	}
+	if content.MsgType == event.MsgImage && content.GetInfo().MimeType == "image/gif" {
 		content.MsgType = event.MsgVideo
 	}
 
