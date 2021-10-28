91e1c5428fd23bfc6cb1847fa5560a692f564020
Tulir Asokan <tulir@maunium.net>
2021-10-28 16:15:08 +0300

    Bump version to v0.1.9

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ var (
 	Name = "mautrix-whatsapp"
 	URL  = "https://github.com/mautrix/whatsapp"
 	// This is changed when making a release
-	Version = "0.1.8"
+	Version = "0.1.9"
 	// This is filled by init()
 	WAVersion = ""
 	VersionString = ""
