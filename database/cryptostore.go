47105b411fdaa3cc2aa89064f8c7e06955a5ccd8
Tulir Asokan <tulir@maunium.net>
2021-10-31 13:04:56 +0200

    Remove legacy build tags

--- database/cryptostore.go
+++ database/cryptostore.go
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 //go:build cgo && !nocrypto
-// +build cgo,!nocrypto
 
 package database
 
--- no-crypto.go
+++ no-crypto.go
@@ -1,5 +1,4 @@
 //go:build !cgo || nocrypto
-// +build !cgo nocrypto
 
 package main
 
