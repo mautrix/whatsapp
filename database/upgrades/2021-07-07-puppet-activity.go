48fd7eaea59482c9351e921a97308f6fe74cace1
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2022-01-05 11:55:06 +0100

    Make puppet-activity DB upgrade sqlite-compatible

--- database/upgrades/2021-07-07-puppet-activity.go
+++ database/upgrades/2021-07-07-puppet-activity.go
@@ -2,15 +2,21 @@ package upgrades
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 func init() {
 	upgrades[21] = upgrade{"Add puppet activity columns", func(tx *sql.Tx, ctx context) error {
-		_, err := tx.Exec(`ALTER TABLE puppet ADD COLUMN IF NOT EXISTS first_activity_ts BIGINT`)
+		addColumn := `ALTER TABLE puppet ADD COLUMN IF NOT EXISTS`
+		if ctx.dialect == SQLite {
+			addColumn = `ALTER TABLE puppet ADD COLUMN`
+		}
+
+		_, err := tx.Exec(fmt.Sprintf(`%s first_activity_ts BIGINT`, addColumn))
 		if err != nil {
 			return err
 		}
-		_, err = tx.Exec(`ALTER TABLE puppet ADD COLUMN IF NOT EXISTS last_activity_ts BIGINT`)
+		_, err = tx.Exec(fmt.Sprintf(`%s last_activity_ts BIGINT`, addColumn))
 		return err
 	}}
 }
