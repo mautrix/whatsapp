e91c70173e5cdd0317714ae9d981f3094dfc753f
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2022-02-17 12:33:38 +0100

    Add a check for whether the expected latest DB update is present

--- check-upgrade-numbers.sh
+++ check-upgrade-numbers.sh
@@ -9,3 +9,11 @@ if [ $UPGRADES -ne $UPGRADES_IDS ]; then
     echo "Take a look at database/upgrades/README.md and increase the numbers recently brought in"
     exit 1
 fi
+
+DECLARED_NUMBER_OF_UPGRADES=`grep -o 'const NumberOfUpgrades = [0-9]*' database/upgrades/upgrades.go | grep -o '[0-9]*'`
+DESIRED_BIGGEST_NUMBER=$(expr $DECLARED_NUMBER_OF_UPGRADES - 1)
+grep "upgrades.$DESIRED_BIGGEST_NUMBER." database/upgrades/* > /dev/null
+if [ $? -eq 1 ]; then
+    echo "Expected to have a DB upgrade number $DESIRED_BIGGEST_NUMBER (for version $DECLARED_NUMBER_OF_UPGRADES) but didn't find one"
+    exit 1
+fi
