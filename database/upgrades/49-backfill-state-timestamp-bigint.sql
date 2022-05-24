a0ffed43ddabc4f97f1d04e652528556949ba6ef
Sumner Evans <sumner@beeper.com>
2022-05-24 09:50:38 -0600

    database/backfill_state: change type of first_expected_ts to BIGINT

---
+++ database/upgrades/49-backfill-state-timestamp-bigint.sql
@@ -0,0 +1,7 @@
+-- v49: Convert first_expected_ts to BIGINT so that we can use zero-values.
+
+-- only: sqlite
+UPDATE backfill_state SET first_expected_ts=unixepoch(first_expected_ts);
+
+-- only: postgres
+ALTER TABLE backfill_state ALTER COLUMN first_expected_ts TYPE BIGINT;
