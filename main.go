45ed657ab1ea01f8028c0842d51d1a9da8213aca
Tulir Asokan <tulir@maunium.net>
2022-07-16 19:50:34 +0300

    Bump version to v0.6.0

--- main.go
+++ main.go
@@ -261,7 +261,7 @@ func main() {
 		Name:         "mautrix-whatsapp",
 		URL:          "https://github.com/mautrix/whatsapp",
 		Description:  "A Matrix-WhatsApp puppeting bridge.",
-		Version:      "0.5.0",
+		Version:      "0.6.0",
 		ProtocolName: "WhatsApp",
 
 		CryptoPickleKey: "maunium.net/go/mautrix-whatsapp",
