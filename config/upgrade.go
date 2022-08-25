46a48541aaa6a21b7d28b4699e9b9185471ca2ef
Tulir Asokan <tulir@maunium.net>
2022-08-25 11:59:09 +0300

    Add config option to use AS transactions for e2be

--- config/upgrade.go
+++ config/upgrade.go
@@ -101,6 +101,7 @@ func DoUpgrade(helper *up.Helper) {
 	helper.Copy(up.Bool, "bridge", "encryption", "allow")
 	helper.Copy(up.Bool, "bridge", "encryption", "default")
 	helper.Copy(up.Bool, "bridge", "encryption", "require")
+	helper.Copy(up.Bool, "bridge", "encryption", "appservice")
 	helper.Copy(up.Str, "bridge", "encryption", "verification_levels", "receive")
 	helper.Copy(up.Str, "bridge", "encryption", "verification_levels", "send")
 	helper.Copy(up.Str, "bridge", "encryption", "verification_levels", "share")
