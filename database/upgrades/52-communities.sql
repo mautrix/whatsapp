8a8a4f9e0176585476ea43c91b3eab2fc0286bb5
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2023-02-27 13:44:47 +0100

    Remove conflicting DB migrations

--- database/upgrades/52-communities.sql
+++
@@ -1,5 +0,0 @@
--- v52: Store portal metadata for communities
-
-ALTER TABLE portal ADD COLUMN is_parent BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE portal ADD COLUMN parent_group TEXT;
-ALTER TABLE portal ADD COLUMN in_space BOOLEAN NOT NULL DEFAULT false;
