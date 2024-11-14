017b3f6c39f539dd872fb69e5cb423b21bcbbd81
@jcardenas4773819 <jcardenas@ikono.com.co>
2024-11-14 11:53:59 -0500

    refactor: :recycle: Git command safe directory removed from build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Git config to use /build as safe directory.
-git config --global --add safe.directory /build
 MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | awk '{ print $2 }')
 GO_LDFLAGS="-s -w -X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
 go build -ldflags="$GO_LDFLAGS" ./cmd/mautrix-whatsapp "$@"
