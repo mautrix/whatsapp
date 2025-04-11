c7f19fe8b1bca61a963a805282b04fff58d1f08c
Tulir Asokan <tulir@maunium.net>
2025-04-11 20:29:13 +0300

    handlewhatsapp: move dont_render_edited to top level

--- pkg/connector/events.go
+++ pkg/connector/events.go
@@ -156,16 +156,15 @@ func (evt *WAMessageEvent) ConvertEdit(ctx context.Context, portal *bridgev2.Por
 			evt.wa.processFailedMedia(ctx, portal.PortalKey, evt.GetID(), cm, false)
 		}
 	}
+	editPart := cm.Parts[0].ToEditPart(existing[0])
 	if evt.isUndecryptableUpsertSubEvent {
-		for _, part := range cm.Parts {
-			if part.Extra == nil {
-				part.Extra = make(map[string]any)
-			}
-			part.Extra["com.beeper.dont_render_edited"] = true
+		if editPart.TopLevelExtra == nil {
+			editPart.TopLevelExtra = make(map[string]any)
 		}
+		editPart.TopLevelExtra["com.beeper.dont_render_edited"] = true
 	}
 	return &bridgev2.ConvertedEdit{
-		ModifiedParts: []*bridgev2.ConvertedEditPart{cm.Parts[0].ToEditPart(existing[0])},
+		ModifiedParts: []*bridgev2.ConvertedEditPart{editPart},
 	}, nil
 }
 
