1385a4f93f9e9e4540adabd0246c497d41857428
Tulir Asokan <tulir@maunium.net>
2025-10-10 01:38:58 +0300

    connector: fix hardcoded_wa_version in log

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -159,6 +159,8 @@ func (wa *WhatsAppConnector) Stop() {
 
 const kvWAVersion = "whatsapp_web_version"
 
+var hardcodedWAVersion = store.GetWAVersion()
+
 func (wa *WhatsAppConnector) onFirstBackgroundConnect() {
 	verStr := wa.Bridge.DB.KV.Get(wa.Bridge.BackgroundCtx, kvWAVersion)
 	if verStr == "" {
@@ -171,7 +173,7 @@ func (wa *WhatsAppConnector) onFirstBackgroundConnect() {
 		return
 	}
 	wa.Bridge.Log.Debug().
-		Stringer("hardcoded_version", store.GetWAVersion()).
+		Stringer("hardcoded_version", hardcodedWAVersion).
 		Stringer("cached_version", ver).
 		Msg("Using cached WhatsApp web version number")
 	store.SetWAVersion(ver)
@@ -184,7 +186,7 @@ func (wa *WhatsAppConnector) onFirstClientConnect() {
 		wa.Bridge.Log.Err(err).Msg("Failed to get latest WhatsApp web version number")
 	} else {
 		wa.Bridge.Log.Debug().
-			Stringer("hardcoded_version", store.GetWAVersion()).
+			Stringer("hardcoded_version", hardcodedWAVersion).
 			Stringer("latest_version", *ver).
 			Msg("Got latest WhatsApp web version number")
 		store.SetWAVersion(*ver)
