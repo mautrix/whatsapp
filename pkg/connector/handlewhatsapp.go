1d68051dae9337a3466de6409b0c8ec86ab73807
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2026-06-09 15:25:21 +0530

    handlewhatsapp: fix HD media caption edits (#927)

--- pkg/connector/handlewhatsapp.go
+++ pkg/connector/handlewhatsapp.go
@@ -363,23 +363,28 @@ func (wa *WhatsAppClient) handleWAMessage(ctx context.Context, evt *events.Messa
 	messageAssoc := evt.Message.GetMessageContextInfo().GetMessageAssociation()
 	if assocType := messageAssoc.GetAssociationType(); assocType == waE2E.MessageAssociation_HD_IMAGE_DUAL_UPLOAD || assocType == waE2E.MessageAssociation_HD_VIDEO_DUAL_UPLOAD {
 		parentKey := messageAssoc.GetParentMessageKey()
-		associatedMessage := evt.Message.GetAssociatedChildMessage().GetMessage()
+		protocolMsg := evt.Message.GetProtocolMessage()
+		if protocolMsg.GetType() != waE2E.ProtocolMessage_MESSAGE_EDIT || protocolMsg.GetKey() == nil {
+			protocolMsg = &waE2E.ProtocolMessage{
+				Type:          waE2E.ProtocolMessage_MESSAGE_EDIT.Enum(),
+				Key:           parentKey,
+				EditedMessage: evt.Message.GetAssociatedChildMessage().GetMessage(),
+			}
+			dontRenderEdited = true
+		} else if child := protocolMsg.GetEditedMessage().GetAssociatedChildMessage().GetMessage(); child != nil {
+			protocolMsg.EditedMessage = child
+			protocolMsg.Key = parentKey
+		}
 		wa.UserLogin.Log.Debug().
 			Str("message_id", evt.Info.ID).
 			Str("parent_id", parentKey.GetID()).
 			Stringer("assoc_type", assocType).
 			Msg("Received HD replacement message, converting to edit")
 
-		protocolMsg := &waE2E.ProtocolMessage{
-			Type:          waE2E.ProtocolMessage_MESSAGE_EDIT.Enum(),
-			Key:           parentKey,
-			EditedMessage: associatedMessage,
-		}
 		evt.Message = &waE2E.Message{
 			ProtocolMessage: protocolMsg,
 		}
 		parsedMessageType = getMessageType(evt.Message)
-		dontRenderEdited = true
 	} else if assocType == waE2E.MessageAssociation_MOTION_PHOTO {
 		//evt.Message = evt.Message.GetAssociatedChildMessage().GetMessage()
 		wa.UserLogin.Log.Debug().
