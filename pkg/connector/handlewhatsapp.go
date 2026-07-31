5152261808d32b6c6e608a6e2109d21db5d64a42
Marcel Manz <coding@marcel.cool>
2026-07-31 13:23:07 +0200

    feat(connector): add keep_revoked_messages option

--- pkg/connector/handlewhatsapp.go
+++ pkg/connector/handlewhatsapp.go
@@ -409,7 +409,7 @@ func (wa *WhatsAppClient) handleWAMessage(ctx context.Context, evt *events.Messa
 		return
 	}
 
-	if parsedMessageType == "revoke" {
+	if parsedMessageType == "revoke" && wa.Main.Config.KeepRevokedMessages {
 		targetID := msgconv.KeyToMessageID(ctx, wa.Client, evt.Info.Chat, evt.Info.Sender, evt.Message.GetProtocolMessage().GetKey())
 		return wa.UserLogin.QueueRemoteEvent(&simplevent.Message[revokeNoticeData]{
 			EventMeta: simplevent.EventMeta{
